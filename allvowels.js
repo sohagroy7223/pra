7d54e5ce94cd4c98073d456124ee7ff8b6d4dc42
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-05 17:24:45 +0600

    change the value of allvowels in js file

--- allvowels.js
+++ allvowels.js
@@ -1,5 +1,22 @@
-let sentence = "Education is a powerful tool ";
-sentence = sentence.toLowerCase();
-let voewls = ["a", "e", "i", "o", "u"];
-for(voewls.every(voewls.))
+// let sentence = "Education is a powerful tool";
+// sentence = sentence.toLowerCase(); // ছোট হাতের অক্ষরে রূপান্তর
 
+// let vowels = ["a", "e", "i", "o", "u"];
+
+// // চেক করা হচ্ছে সব vowel আছে কিনা
+// let hasAllVowels = vowels.every((vowel) => sentence.includes(vowel));
+
+// if (hasAllVowels) {
+//     console.log("The string contains all the vowels (a, e, i, o, u).");
+// } else {
+//     console.log("The string does not contain all the vowels.");
+// }
+
+// or******OR******////
+
+let sentence = "Education is a powerful tool";
+let vowels = ["a", "e", "i", "o", "u"];
+
+let hasAllVowels = vowels.every((vowel) => sentence.includes(vowel));
+
+console.log(hasAllVowels);
