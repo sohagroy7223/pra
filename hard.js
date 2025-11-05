e090a97b495bdaed2752cc3cadb9ad100d118b48
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-05 16:49:20 +0600

    addad hard task in js

---
+++ hard.js
@@ -0,0 +1,14 @@
+let myObject = {
+    name: "John Doe",
+    age: 25,
+    city: "Example City",
+    isStudent: true,
+};
+
+// for (let key in myObject) {
+//     console.log(`key: ${key} | type: ${typeof myObject[key]}`);
+// }
+
+for (let sohag in myObject) {
+    console.log(`key: ${sohag} | type: ${typeof myObject[sohag]} `);
+}
